dc5cec2d5bb4f41e7dcc1f7a519d37687f22479f
FishShalami <fishshalami@gmail.com>
2025-04-25 10:59:54 -0700

    Developing work experience component and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
 import { useState } from "react";
 import "./App.css";
 import PersonalInfo from "./PersonalInfo";
+import WorkExperience from "./WorkExperience";
 
 function App() {
   const [contactData, setContactData] = useState({
-    firstName: "",
-    lastName: "",
-    phone: "",
-    email: "",
+    firstName: "Your",
+    lastName: "Name",
+    phone: "123-456-7890",
+    email: "xyz@gmail.com",
   });
 
   const fullName = `${contactData.firstName} ${contactData.lastName}`;
 
+  const [workData, setWorkData] = useState({
+    companyName: "Company 1",
+    positionTitle: "Manager",
+    startDate: "2020-04-09",
+    endDate: "2025-04-09",
+    positionLocation: "Baltimore, MD",
+    positionDescription:
+      "Led a team of 10 in the development of a new product line.",
+  });
+
   const handleContactChange = (e) => {
     const { name, value } = e.target;
     setContactData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleWorkExperienceChange = (e) => {
+    const { name, value } = e.target;
+    setWorkData((prev) => ({
       ...prev,
       [name]: value,
     }));
@@ -30,23 +49,41 @@ function App() {
               contactData={contactData}
               handleContactChange={handleContactChange}
             />
-            <PersonalInfo
-              contactData={contactData}
-              handleContactChange={handleContactChange}
+            <WorkExperience
+              workData={workData}
+              handleWorkExperienceChange={handleWorkExperienceChange}
             />
           </form>
         </div>
         <div className="outputs">
-          {contactData.firstName !== "" && <h1>{fullName}</h1>}
-          {(contactData.phone || contactData.email) && (
-            <p>
-              <span>
-                {contactData.phone}
-                {contactData.phone && contactData.email && " | "}
-                {contactData.email}
-              </span>
-            </p>
-          )}
+          <div className="cv-header">
+            {contactData.firstName !== "" && <h1>{fullName}</h1>}
+            {(contactData.phone || contactData.email) && (
+              <p>
+                <span>
+                  {contactData.phone}
+                  {contactData.phone && contactData.email && " | "}
+                  {contactData.email}
+                </span>
+              </p>
+            )}
+          </div>
+          <div className="work-experience-header">
+            <h2>Work Experience</h2>
+            <hr className="solid" />
+            <div className="work-experence-body">
+              <h3>{workData.companyName}</h3>
+              <p>
+                <b>{workData.positionTitle}</b>
+                <br />
+                {workData.startDate} - {workData.endDate}
+                <br />
+                {workData.positionLocation}
+                <br />
+                {workData.positionDescription}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/WorkExperience.jsx
+++ src/WorkExperience.jsx
@@ -1,37 +1,57 @@
-function Work Experience({ contactData, handleContactChange }) {
+function WorkExperience({ workData, handleWorkExperienceChange }) {
   return (
     <fieldset>
       <legend>Work Experience</legend>
-      <div className="pi">
-        <div className="pi-row1">
-          <input
-            type="text"
-            name="companyName"
-            id="companyName"
-            placeholder="Company Name"
-            value={contactData.firstName}
-            onChange={handleContactChange}
-          />
-          <input
-            type="text"
-            name="postionTitle"
-            id="postionTitle"
-            placeholder="Your Position"
-            value={contactData.lastName}
-            onChange={handleContactChange}
-          />
-          <input type="date" name="startDate" id="startDate" />
-          <input type="date" name="endDate" id="endDate" />
-          <input type="text" name="positionLocation" id="positionLocation" />
-          <input
-            type="text"
-            name="positionDescription"
-            id="positionDescription"
-          />
-        </div>
+      <div className="we">
+        <input
+          type="text"
+          name="companyName"
+          id="companyName"
+          placeholder="Company Name"
+          value={workData.companyName}
+          onChange={handleWorkExperienceChange}
+        />
+        <input
+          type="text"
+          name="positionTitle"
+          id="positionTitle"
+          placeholder="Your Position"
+          value={workData.positionTitle}
+          onChange={handleWorkExperienceChange}
+        />
+        <input
+          type="date"
+          name="startDate"
+          id="startDate"
+          value={workData.startDate}
+          onChange={handleWorkExperienceChange}
+        />
+        <input
+          type="date"
+          name="endDate"
+          id="endDate"
+          value={workData.endDate}
+          onChange={handleWorkExperienceChange}
+        />
+        <input
+          type="text"
+          name="positionLocation"
+          id="positionLocation"
+          placeholder="Location"
+          value={workData.positionLocation}
+          onChange={handleWorkExperienceChange}
+        />
+        <input
+          type="text"
+          name="positionDescription"
+          id="positionDescription"
+          placeholder="Position Description"
+          value={workData.positionDescription}
+          onChange={handleWorkExperienceChange}
+        />
       </div>
     </fieldset>
   );
 }
 
-export default PersonalInfo;
+export default WorkExperience;
